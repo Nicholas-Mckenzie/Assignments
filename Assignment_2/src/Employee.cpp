4144bdc3a8131a6361be142a0c5cdeb99987e401
Nicholas-Mckenzie <136575013+Nicholas-Mckenzie@users.noreply.github.com>
2023-06-29 18:51:58 -0500

    Add files via upload

---
+++ Assignment_2/src/Employee.cpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "Employee.h"
+
+
+
+
+Employee::Employee(std::string name)
+	: m_name(name), unexcusedAbsences(0)
+{
+}
+
+void Employee::printMainInfo() const
+{
+	std::cout << "Employee: ";
+	printName();
+	std::cout << "Unexcused Absences: " << getUnexcusedAbsences() << std::endl;
+	std::cout << "Weekly Salary: " << weeklySalary() << std::endl;
+	std::cout << "HealthContributions: " << healthContribution() << std::endl;
+	std::cout << "Vacation Days: " << vacationDays() << std::endl;
+}
+
+void Employee::printName() const
+{
+	std::cout << m_name << std::endl;
+}
+
+void Employee::printAbscences() const
+{
+	std::cout << m_name << " has " << unexcusedAbsences << std::endl;
+}
+
+void Employee::incrementAbsence()
+{
+	unexcusedAbsences++;
+}
+
+size_t Employee::getUnexcusedAbsences() const
+{
+	return unexcusedAbsences;
+}
+
+
+
+
